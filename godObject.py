ec511cccec3dc67920e8f404b605f95718e4406f
kanishk779 <kashyapamankumar54@gmail.com>
2020-01-12 10:28:02 +0530

    Add GodObject.py

---
+++ godObject.py
@@ -0,0 +1,25 @@
+from colorama import Fore, Back, Style
+import numpy as np
+class location:
+    
+    def __init__(self,x_len=0,y_len=0,x_loc=0,y_loc=0):
+        self.__x_loc = x_loc
+        self.__y_loc = y_loc
+        self.__x_length = x_len
+        self.__y_length = y_len
+
+
+
+class GodObject:
+    
+    def __init__(self,obj_location,identifier):
+        self.obj_location = obj_location
+        self.__identifier = identifier
+
+
+    def setIdentifier(identifier):
+        self.__identifier = identifier
+
+    def getIdentifier():
+        return self.__identifier
+    
